c6edbd1accc8cc87db7dbad99c8a333fac03a26b
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-10-17 11:59:19 +0300

    aprovement

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -2,18 +2,26 @@ import { observer } from './observer.js';
 import COMMONS from '../modules/commons.js';
 import Notiflix from 'notiflix';
 
+function noPhotoMsg() {
+  COMMONS.loader.classList.add('visually-hidden');
+  Notiflix.Notify.failure(`Упс, більше ми не маємо фото по цьому запросу. `);
+  const erorItem = `<div style=" margin: 0 auto; margin-top: 75px; width: 1200px;"> <img src="https://static.thenounproject.com/png/1269202-200.png"
+   style=" margin: 0 auto; display: flex;" alt="placeholder" width="400"/></div>`;
+  return COMMONS.container.insertAdjacentHTML('beforeend', erorItem);
+}
+
 function onError(error) {
+  if (error.response.data === '[ERROR 400] "page" is out of valid range.') {
+    noPhotoMsg();
+    return observer.unobserve(COMMONS.guard);
+  }
   COMMONS.loader.classList.add('visually-hidden');
   Notiflix.Notify.failure(`Загрузка неможлива, ${error} 🤷‍♂️`);
   return observer.unobserve(COMMONS.guard);
 }
 
 function emptyResponse() {
-  COMMONS.loader.classList.add('visually-hidden');
-  Notiflix.Notify.failure(`Упс, щось пішло не так ☠️. Введіть щось інше. `);
-  const erorItem = `<div style=" margin: 0 auto; margin-top: 75px; width: 1200px;"> <img src="https://static.thenounproject.com/png/1269202-200.png"
-   style=" margin: 0 auto; display: flex;" alt="placeholder" width="400"/></div>`;
-  COMMONS.container.insertAdjacentHTML('beforeend', erorItem);
+  noPhotoMsg();
   return observer.unobserve(COMMONS.guard);
 }
 
